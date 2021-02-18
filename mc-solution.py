3b7be3cae132f041c9a42cf123ad0c5c1dfb3cac
LuisChore <luis.vazquez.ch@hotmail.com>
2021-02-18 07:52:17 -0600

    individual states and action for each agent

--- mc-solution.py
+++ mc-solution.py
@@ -4,13 +4,11 @@ from multiAgentEnv import MultiCast
 from Graph import Graph
 
 
-episodes = 100000
+episodes = 10
 gamma = 1 # discount factor
 eps = 0.5
 
 inf = float('inf')
-
-
 
 def print_policy(index,graph,policy):
     for v,_ in graph.adj[index]:
@@ -80,45 +78,39 @@ def play_iteration(env,policy,render = False, answer = False):
     else:
         return states_actions_returns
 
-
-def modify_policy(PI,s,a,value):
-    for si,ai in zip(s,a):
-        if (si,ai) in PI:
-            PI[(si,ai)] = min(PI[(si,ai)],value)
-
 def main():
     f = open("example.txt", "r")
     nodes,edges = f.readline().split()
     g = Graph(int(nodes),True)
-    PI = {}
+    Q = {}
+    Qc = {}
     for i in range(int(edges)):
         u,v,w = f.readline().split()
         g.add_edge(int(v),int(u),int(w)) #reversed, from agents to target
-        PI[(int(v),int(u))] = inf
+        Q[(int(v),int(u))] = inf
+        Qc[(int(v),int(u))] = 0
     agents = [int(x) for x in f.readline().split()]
     target = int(f.readline())
     env = MultiCast(g,agents,target)
-    Q = {}
-    Qc = {}
     for episode in range(episodes):
-        states_actions_returns = play_iteration(env,PI)
+        states_actions_returns = play_iteration(env,Q)
         for s,a,G in states_actions_returns:
-            if s in Q:
-                if a in Q[s]:
-                    qc = Qc[s][a]
-                    Q[s][a] = (Q[s][a]*qc + G) / (qc + 1)
-                    Qc[s][a] += 1
-                else:
-                    Q[s][a] = G
-                    Qc[s][a] = 1
-            else:
-                Q[s] = {}
-                Qc[s] = {}
-                Q[s][a] = G
-                Qc[s][a] = 1
-            modify_policy(PI,s,a,Q[s][a])
+            for si,ai in zip(s,a):
+                if (si,ai) in Q:
+                    print("->" + str((si,ai)))
+                    if Qc[(si,ai)] == 0:
+                        Q[(si,ai)] = G
+                        Qc[(si,ai)] = 1
+                    else:
+                        qc = Qc[(si,ai)]
+                        Q[(si,ai)] = (Q[(si,ai)]*qc + G) / (qc + 1)
+                        Qc[(si,ai)] += 1
+
+        for u in range(0,g.nodes):
+            for v,_ in g.adj[u]:
+                print( str((u,v)) + " : " + str(Q[(u,v)]))
     eps = 0
-    G = play_iteration(env,PI,True,True)
+    G = play_iteration(env,Q,True,True)
     print("Solution: " + str(G))
 if __name__ == "__main__":
     main()
